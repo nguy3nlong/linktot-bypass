8ea5c7b48e667eaf49040c96f360e0e8b3e34016
Reno <longnguyenstocks@gmail.com>
2025-07-17 08:06:48 +0700

    Update linktot.py

--- linktot.py
+++ linktot.py
@@ -2,21 +2,34 @@ import base64
 import requests
 import random
 import time
+import re
 rod = random.randint(100000, 999999)
 rad = str(rod)
 
+unix_time = int(time.time())
 
 url = input('Enter quest url: ')
-
-
-headers = {
+lurl = input('Enter Linktot url: ')
+type = input('Enter quest type: (e.g backlink, normal, changecolor): ')
+if type == 'normal':
+    headers = {
     'origin': url,
     'referer': url,
     'rid': rad,
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Safari/605.1.15',
 }
 
-response = requests.options('https://linktot.net/ping.php', headers=headers)
+    response = requests.options('https://linktot.net/ping.php', headers=headers)
+
+if type == 'backlink':
+    headers = {
+    'origin': url,
+    'referer': url,
+    'rid': rad,
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Safari/605.1.15',
+}
+
+    response = requests.options('https://linktot.net/ping_backlink.php', headers=headers)
 print('Pending...')
 if response.status_code == 200:
     time.sleep(100)
